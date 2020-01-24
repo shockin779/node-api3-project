2708f1f13e72b8031c284382176a34e9e5bbf578
Sean Hockin <sean.hockin@gmail.com>
2020-01-24 12:01:39 -0700

    MVP complete

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter');
 const server = express();
+
+server.use(express.json());
+server.use(logger);
+
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +13,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} request to ${req.url}.`);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,124 @@
 const express = require('express');
+const Users = require('./userDb');
+const Posts = require('../posts/postDb');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal Server error. Cannot add user.', error: err})
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  req.newPost.user_id = req.params.id;
+  Posts.insert(req.newPost)
+    .then(recordsAdded => {
+      res.status(201).json(recordsAdded);
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal Server error. Cannot add post to user.', error: err})
+    })
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get()
+    .then(users => {
+      res.status(200).json({users});
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal Server error. Cannot get users.', error: err})
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  const {id} = req.params;
+  Users.getUserPosts(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal Server error. Cannot get users posts.', error: err})
+    })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  Users.remove(req.user.id)
+    .then(recordsAdded => {
+      res.status(204).json(recordsAdded);
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal Server error. Cannot delete user.', error: err})
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const changes = req.body;
+  Users.update(req.user.id, changes)
+    .then(recordsChanged => {
+      res.status(200).json(recordsChanged);
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal Server error. Cannot update user.', error: err})
+    })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const {id} = req.params;
+  Users.getById(id)
+    .then(user => {
+      if(user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({message: 'invalid user id'});
+      }
+    })
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  let bodyCount = Object.keys(req.body);
+  if(bodyCount.length < 1) {
+    res.status(400).json({ message: "missing user data" });
+  } else if(!req.body.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  let bodyCount = Object.keys(req.body);
+  if(bodyCount < 1) {
+    res.status(400).json({ message: "missing post data" });
+  } else if(!req.body.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    req.newPost = {
+      text: req.body.text,
+    }
+    next();
+  }
 }
 
 module.exports = router;
